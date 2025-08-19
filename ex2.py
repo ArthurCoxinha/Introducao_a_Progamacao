c55427ba69886cf0338928d4e66686569fb45a22
ArthurCoxinha <augustoarthur501@gmail.com>
2025-08-19 08:40:21 -0300

    Add files via upload

---
+++ ex2.py
@@ -0,0 +1,4 @@
+nota1 = float(input("Primeira nota: "))
+nota2 = float(input("Segunda nota: "))
+notaquefalta = 60 - (nota1*2 + nota2*2)
+print("Você precisa tirar ",((notaquefalta/2)/3), "nos próximos 2 bimestres")
---
+++ ex3.py
@@ -0,0 +1,3 @@
+ng = float(input("número multiplicado:"))
+for nt in range(10):
+ print(ng, "X", 1+nt,"=", ((1+nt)*ng))
