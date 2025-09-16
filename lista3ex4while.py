da044121326568c1ab64f1acddfff422d3103cb5
ArthurCoxinha <augustoarthur501@gmail.com>
2025-09-16 08:32:09 -0300

    Delete lista3ex4while.py

--- lista3ex4while.py
+++
@@ -1,8 +0,0 @@
-contagem = 0
-somaAltura = 0
-jogadores = int(input("número de jogadores "))
-while contagem!=jogadores: 
-    altura = float(input("digite a altura dos jogadores: "))
-    somaAltura+=altura
-    contagem+=1
-print("a altura média dos jogadores é:", somaAltura/jogadores)
