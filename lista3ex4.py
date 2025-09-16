429e150361bcae19bf09431499f0caefeadc47ef
ArthurCoxinha <augustoarthur501@gmail.com>
2025-09-16 08:31:51 -0300

    Delete lista3ex4.py

--- lista3ex4.py
+++
@@ -1,7 +0,0 @@
-jogadores = 0
-somaAltura = 0
-jogadores = int(input("número de jogadores "))
-for i in range(jogadores):
-    altura = float(input("digite a altura dos jogadores: "))
-    somaAltura+=altura
-print("a altura média dos jogadores é:", somaAltura/jogadores)
