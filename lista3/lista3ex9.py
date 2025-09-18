5bf54e43e9bf8b458534ddbea7e6bdb59cebcf04
ArthurCoxinha <augustoarthur501@gmail.com>
2025-09-18 10:26:20 -0300

    Update lista3ex9.py

--- lista3/lista3ex9.py
+++ lista3/lista3ex9.py
@@ -2,12 +2,9 @@ numero = int(input("digite o numero para ver se é primo ou não: "))
 contagem = 0
 if numero<2:
     print("o numero não é primo: ")
-    
-for i in range(1, numero + 1):
-        
+for i in range(1, numero + 1):  
     if numero %i == 0:
-        contagem += 1
-            
+        contagem += 1            
 if contagem ==2:
     print("é numero primo")
 else:
